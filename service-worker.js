33f24a056b38678821bab8d701f90667e1374ddd
pawansutar3 <64617951+pawansutar3@users.noreply.github.com>
2020-11-26 23:57:48 +0000

    Deploying to gh-pages from  @ 3f5e9f2a9ece794e90de4e8ee38dc6f5c9fbc68a 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e10382c1c1dbfa1b06fdcdee2325b108.js"
+  "/precache-manifest.54a3b7fa6fc95f8e9512987eb565fab9.js"
 );
 
 self.addEventListener('message', (event) => {
